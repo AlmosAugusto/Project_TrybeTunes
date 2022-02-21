87f02f45ab6a9da1408d63c1e572a3fe04543c44
almosAugusto <almos.filho@hotmail.com>
2022-02-21 17:14:02 -0300

    Task 12 (incomplete and fix)

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import Header from '../Components/Header';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 export default class Favorites extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      /* loading: false,
+      isFavoriteChecked: false,
+      favoriteSongs: [], */
+    };
+  }
+
+  componentDidMount() {
+    this.getFavoriteFunc();
+  }
+
+  getFavoriteFunc = async () => {
+    const listOfFavoriteSongs = await getFavoriteSongs();
+    console.log(listOfFavoriteSongs);
+  }
+
   render() {
     return (
       <div data-testid="page-favorites">
